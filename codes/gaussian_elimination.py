0e1a89e810d1ab82e5c882da348525e2d2bd3307
ramtinmk <ramtinmk1382@gmail.com>
2023-08-05 15:32:18 +0330

    added matrix scaling for gaussian elimination

--- codes/gaussian_elimination.py
+++ codes/gaussian_elimination.py
@@ -1,8 +1,18 @@
 import numpy as np
+from solving_linear_system_LU import backward_substitution
 
+def scaling(matrix):
+    max_values = np.max(matrix, axis=1, keepdims=True)
+    # Divide each row by its corresponding maximum value using broadcasting
+    matrix = matrix / max_values
+    return matrix
+    
 def gaussian_elimination(matrix):
     m, n = matrix.shape
+    
 
+    
+    matrix = scaling(matrix)
     for i in range(min(m, n)):
         # Find the maximum row in the current column and swap
         max_row = np.argmax(np.abs(matrix[i:, i])) + i
@@ -16,7 +26,7 @@ def gaussian_elimination(matrix):
         for k in range(i + 1, m):
             factor = matrix[k, i]
             matrix[k, :] -= factor * matrix[i, :]
-        # Eliminate non-zero elements above the diagonal
+        #Eliminate non-zero elements above the diagonal
         for l in range(i):
           factor = matrix[l,i]
           matrix[l,:] -= factor * matrix[i,:]
@@ -27,12 +37,13 @@ def gaussian_elimination(matrix):
 if __name__ == "__main__":
     # Example system of linear equations in matrix form (Ax = B)
     A = np.array([
-        [2, 3, -1],
-        [4, 1, -2],
-        [1, 2, 1],
-        [3, 4, 2]
+        [1, -1, 1,1],
+        [2, 7, -1,8],
+        [15,1, -8,8]
     ], dtype=float)
 
     # Perform forward elimination
     A = gaussian_elimination(A)
     print("Matrix after forward elimination:\n", A)
+    
+
